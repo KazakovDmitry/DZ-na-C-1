4ee8443f8aa134b6842e68c0acb1fcd88e4116e3
Dmitry <dimakameya@gmail.com>
2023-01-16 16:17:41 +0500

    Чётные числа от 1 до N

---
+++ Task8/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+Console.WriteLine("Введите число: ");
+string numbersA = Console.ReadLine();
+int a = Convert.ToInt32(numbersA);
+
+Console.WriteLine("Чётные числа от 1 до " + a);
+for (int i = 2; i <= a; i = i + 2)
+{
+    Console.Write(i + " ");
+}
